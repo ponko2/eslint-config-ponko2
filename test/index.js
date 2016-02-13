c2513ead1c80e52e96a2fa6ba49ef54df8ad7fea
Takahito Nakano <kano@ponko2.jp>
2016-02-13 22:45:28 +0900

    テストを追加

---
+++ test/index.js
@@ -0,0 +1,45 @@
+import test        from 'ava';
+import {CLIEngine} from 'eslint';
+import eslintrc    from '../';
+
+const validCode = (
+`'use strict';
+
+const message = 'Hello, World!';
+
+if (message !== '') {
+  console.log(message);
+}
+`);
+
+const invalidCode = (
+`'use strict';
+
+const message = 'Hello, World!';
+
+if (message != '') {
+  console.log(message);
+}
+`);
+
+const cli = new CLIEngine({
+  'useEslintrc': false,
+  'baseConfig': eslintrc,
+  'envs': ['browser'],
+  'rules': {'no-console': 0}
+});
+
+test('no warnings with valid code', t => {
+  const result = cli.executeOnText(validCode).results[0];
+
+  t.notOk(result.warningCount, 'no warnings');
+  t.notOk(result.errorCount, 'no errors');
+  t.same(result.messages, [], 'no messages in results');
+});
+
+test('a warning with invalid code', t => {
+  const result = cli.executeOnText(invalidCode).results[0];
+
+  t.ok(result.errorCount, 'fails');
+  t.is(result.messages[0].ruleId, 'eqeqeq');
+});
