347d94327d44061638e390898200e66cd42f6be3
Takahito Nakano <kano@ponko2.jp>
2025-12-31 02:54:09 +0900

    chore(config): Update flake.nix

--- flake.nix
+++ flake.nix
@@ -16,24 +16,28 @@
         "x86_64-linux"
       ];
       perSystem =
-        { pkgs, ... }:
-        let
-          packageJSON = pkgs.lib.importJSON ./package.json;
-          nodejs =
-            pkgs."nodejs_${pkgs.lib.versions.major (pkgs.lib.removePrefix "^" packageJSON.devEngines.runtime.version)}";
-          pnpm = pkgs.runCommand "pnpm" { buildInputs = [ nodejs ]; } ''
-            mkdir -p $out/bin
-            corepack enable pnpm --install-directory=$out/bin
-          '';
-        in
+        { pkgs, system, ... }:
         {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              (final: _prev: {
+                pnpm = final.runCommand "pnpm" { buildInputs = [ final.corepack ]; } ''
+                  mkdir -p $out/bin
+                  corepack enable pnpm --install-directory=$out/bin
+                '';
+              })
+            ];
+          };
           devShells.default = pkgs.mkShellNoCC {
             packages = with pkgs; [
               editorconfig-checker
-              nodejs
               pnpm
               yamllint
             ];
+            shellHook = ''
+              pnpm install
+            '';
           };
           formatter = pkgs.nixfmt-tree;
         };
